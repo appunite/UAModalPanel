77138b3be6f02060db4245462f85ce62b47f1e04
Karol Wojtaszek <karol@appunite.com>
2012-08-27 10:36:44 +0200

    added static library

---
+++ UAModalPanelLib/UAModalPanelLib.h
@@ -0,0 +1,13 @@
+//
+//  UAModalPanelLib.h
+//  UAModalPanelLib
+//
+//  Created by Karol Wojtaszek on 02.08.2012.
+//  Copyright (c) 2012 Urban Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UAModalPanelLib : NSObject
+
+@end
---
+++ UAModalPanelLib/UAModalPanelLib.m
@@ -0,0 +1,13 @@
+//
+//  UAModalPanelLib.m
+//  UAModalPanelLib
+//
+//  Created by Karol Wojtaszek on 02.08.2012.
+//  Copyright (c) 2012 Urban Apps. All rights reserved.
+//
+
+#import "UAModalPanelLib.h"
+
+@implementation UAModalPanelLib
+
+@end
